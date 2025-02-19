5edf68723476ede3ea0c67015e67ceb22b2c22fc
Nikita <nikitos54345@gmail.com>
2025-02-19 11:19:13 +0300

    change wrong variants

--- lab1/task3.py
+++ lab1/task3.py
@@ -1,5 +1,7 @@
 arr = input('Введите все элементы массива разделяя их пробелами\n').split(' ')
+answer = []
 print('чётные элементы:')
 for x in range(0, len(arr)):
     if x & 1 == 1:
-        print(arr[x])
+        answer.append(arr[x])
+print(' '.join(answer))
--- lab2/task7.py
+++ lab2/task7.py
@@ -3,4 +3,6 @@ table = PrettyTable()
 table.field_names = ['Ф', 'И', 'О', 'о студенте']
 table.add_row(['Иванов', 'Иван', 'Иванович', 'Студент 3 курса, 23 года'])
 table.add_row(['Петров', 'Семен', 'Игоревич', 'Студент 2 курса, 22 года'])
+table.border = False
+table.align = 'l'
 print(table)
