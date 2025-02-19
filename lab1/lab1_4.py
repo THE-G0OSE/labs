e327f417243159009a709ea873eab3aeaa28b328
Nikita <nikitos54345@gmail.com>
2025-02-19 10:44:35 +0300

    some fixes

--- lab1/lab1_4.py
+++ lab1/lab1_4.py
@@ -2,5 +2,8 @@ arr = list(map(int, input('Введите числа разделяя пробе
 answer = 0
 for i in range(len(arr)):
     if arr[i] < 10:
-        answer+=arr[i]
+        if answer == 0:
+            answer+=arr[i]
+        else: 
+            answer *= arr[i]
 print(answer)
--- lab2/task1.py
+++ lab2/task1.py
@@ -3,4 +3,4 @@ my_number = round(random.random() * 100)
 a = my_number
 while a == my_number:
     a = input('введите число отличное от загаданного\n')
-print('Отлично, загаданное число было равно', my_number)
+print('Отлично, загаданное число было равно ', my_number)
