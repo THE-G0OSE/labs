88ca6a73f30595678374e37a7d0f6e483da369b2
Nikita <nikitos54345@gmail.com>
2025-03-05 10:37:49 +0300

    lab1 task7 issue

--- lab2/task7.py
+++ lab2/task7.py
@@ -1,8 +1,15 @@
 from prettytable import PrettyTable
 table = PrettyTable()
-table.field_names = ['Ф', 'И', 'О', 'о студенте']
-table.add_row(['Иванов', 'Иван', 'Иванович', 'Студент 3 курса, 23 года'])
-table.add_row(['Петров', 'Семен', 'Игоревич', 'Студент 2 курса, 22 года'])
+string = 'Ф;И;О;Возраст;Категория;_Иванов;Иван;Иванович;23 года;Студент 3 курса;_Петров;Семен;Игоревич;22 года;Студент 2 курса'
+rows = string.split('_')
+headersRaw = rows[0].split(';')
+headers = headersRaw[:3]
+headers.append('О студенте')
+table.field_names = headers[:4]
+for i in rows[1:]:
+    row = i.split(';')
+    table.add_row([row[0],row[1],row[2],' '.join(row[3:5])])
 table.border = False
 table.align = 'l'
+table.align['О студенте'] = 'c'
 print(table)
